6c90ec323f19e0707a169c01195bd473e738eba1
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-18 20:09:14 +0530

    feat: add Train entity and ticketsBooked field to User

--- app/src/main/java/booker/entities/User.java
+++ app/src/main/java/booker/entities/User.java
@@ -1,5 +1,7 @@
 package booker.entities;
 
+import java.util.List;
+
 public class User {
 
     private String name;
@@ -8,7 +10,7 @@ public class User {
 
     private String hashPassword;
 
-   // private List<Ticket> ticketsBooked;
+    private List<Ticket> ticketsBooked;
 
     private String userId;
 }
