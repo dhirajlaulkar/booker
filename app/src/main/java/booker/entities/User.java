9ae08521174425da585f9851c26b6216ca2c47cb
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-18 19:59:38 +0530

    chore: set up Gradle wrapper, configure line endings, add core domain classes and tests

---
+++ app/src/main/java/booker/entities/User.java
@@ -0,0 +1,14 @@
+package booker.entities;
+
+public class User {
+
+    private String name;
+
+    private String password;
+
+    private String hashPassword;
+
+   // private List<Ticket> ticketsBooked;
+
+    private String userId;
+}
