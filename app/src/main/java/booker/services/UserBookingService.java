2e35e98e601f64f44fd16b55fcfa804cec4b466e
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-20 00:29:32 +0530

    refactor: load users from JSON in UserBookingService using Jackson

--- app/src/main/java/booker/services/UserBookingService.java
+++ app/src/main/java/booker/services/UserBookingService.java
@@ -1,15 +1,25 @@
 package booker.services;
 
 import booker.entities.User;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class UserBookingService  {
 
     private User user;
+    private List<User> userList;
+    private ObjectMapper objectMapper =new ObjectMapper();
 
-    public UserBookingService(User user1){
+    private final String USERS_PATH ="../localDb/users.json";
+
+    public UserBookingService(User user1) throws IOException {
         this.user=user1;
-        File users=new File();
+        File users=new File(USERS_PATH);
+        userList= objectMapper.readValue(users, new TypeReference<List<User>>() {});
     }
 
 }
