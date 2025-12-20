0490039e2cc603fe7beee814795a1192f315b2ac
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-20 17:25:13 +0530

    refactor: add getters and setters for ticket fields

--- app/src/main/java/booker/entities/Ticket.java
+++ app/src/main/java/booker/entities/Ticket.java
@@ -27,4 +27,51 @@ public class Ticket {
         return String.format("Ticket ID: %s belongs to User %s from %s to 5s on %s", ticketId,userId,source,destination,dateOfTravel);
     }
 
+    public String getTicketId() {
+        return ticketId;
+    }
+
+    public void setTicketId(String ticketId) {
+        this.ticketId = ticketId;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getDateOfTravel() {
+        return dateOfTravel;
+    }
+
+    public void setDateOfTravel(String dateOfTravel) {
+        this.dateOfTravel = dateOfTravel;
+    }
+
+    public Train getTrain() {
+        return train;
+    }
+
+    public void setTrain(Train train) {
+        this.train = train;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 }
