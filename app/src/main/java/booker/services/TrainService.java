d68d66a57db9bef438af110427939387cd3e47b7
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-24 23:58:40 +0530

    feat: add train search to TrainService

--- app/src/main/java/booker/services/TrainService.java
+++ app/src/main/java/booker/services/TrainService.java
@@ -1,4 +1,14 @@
 package booker.services;
 
+import booker.entities.Train;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class TrainService {
+
+    public List<Train> searchTrains(String source, String destination) {
+        return trainList.stream().filter(train -> validateTrain(train, source, destination))
+                .collect(Collectors.toList());
+    }
+
 }
