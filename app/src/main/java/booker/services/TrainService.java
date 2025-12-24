17bc742e31d0226333dc7a5991d8953162f1d5c1
githubdhiraj <dhirajlaulkar101@gmail.com>
2025-12-25 01:10:33 +0530

    refactor: load train data from JSON and define constructor

--- app/src/main/java/booker/services/TrainService.java
+++ app/src/main/java/booker/services/TrainService.java
@@ -1,17 +1,37 @@
 package booker.services;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import booker.entities.Train;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.OptionalInt;
+
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class TrainService {
 
+    private List<Train> trainList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TRAIN_DB_PATH = "../localDB/trains.json";
+
+    public TrainService() throws IOException {
+        File trains = new File(TRAIN_DB_PATH);
+
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {
+        });
+    };
+
     public List<Train> searchTrains(String source, String destination) {
-        return trainList.stream().filter(train -> validateTrain(train, source, destination))
+        return trainList.stream().filter(train -> validTrain(train, source, destination))
                 .collect(Collectors.toList());
     }
 
-    private boolean validateTrain(Train train, String source, String destination) {
+    private boolean validTrain(Train train, String source, String destination) {
         List<String> stations = train.getStations();
         int sourceIndex = stations.indexOf(source.toLowerCase());
         int destinationIndex = stations.indexOf(destination.toLowerCase());
